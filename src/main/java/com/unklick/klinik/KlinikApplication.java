6b71ff6fbf4d1bfaec8cb5b75e94a2f1d0ffda61
sapinodejs@gmail.com <>
2019-11-23 23:31:52 +0100

    initial project add

---
+++ src/main/java/com/unklick/klinik/KlinikApplication.java
@@ -0,0 +1,13 @@
+package com.unklick.klinik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlinikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlinikApplication.class, args);
+	}
+
+}
