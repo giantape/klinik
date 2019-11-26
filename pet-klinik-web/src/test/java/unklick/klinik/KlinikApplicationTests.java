c4a8b384282c2da21b52651dcdd5e5f27b74636f
sapinodejs@gmail.com <>
2019-11-26 23:45:30 +0100

    refactoring to multimodule maven project. closes #1

---
+++ pet-klinik-web/src/test/java/unklick/klinik/KlinikApplicationTests.java
@@ -0,0 +1,16 @@
+package unklick.klinik;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class KlinikApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
