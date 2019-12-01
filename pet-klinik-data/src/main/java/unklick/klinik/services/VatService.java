550dba2a5166350f221096d5acbb344d67a358ad
sapinodejs@gmail.com <>
2019-12-01 07:03:44 +0100

    refactor common crud interface. closes #11

--- pet-klinik-data/src/main/java/unklick/klinik/services/VatService.java
+++ pet-klinik-data/src/main/java/unklick/klinik/services/VatService.java
@@ -2,10 +2,4 @@ package unklick.klinik.services;
 
 import unklick.klinik.model.Vat;
 
-import java.util.Set;
-
-public interface VatService {
-    Vat findById(Long id);
-    Vat save(Vat vat);
-    Set<Vat> findAll();
-}
+public interface VatService extends CrudService<Vat, Long>{}
